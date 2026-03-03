9288f00cb7595074482f22763bd606c30b34a2f4
ajay-k-4847 <ajayrocks5668@gmail.com>
2026-03-03 09:07:59 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPE_E_DOT = '-e .'
+def get_requirements(file_path:str) -> List[str]:
+    """
+        This function returns list of requirements
+    """
+    requirements = []
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+    if HYPE_E_DOT in requirements:
+        requirements.remove(HYPE_E_DOT)
+
+    return requirements
+
+
+setup(
+name = 'mlproject',
+version='0.0.1',
+author='Ajay',
+author_email='ajayrocks5668@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
